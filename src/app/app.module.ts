5bd9827138ce727810dcfa450ef1378e91aa2636
DESKTOP-KP43KU2\Gilbert Alexandre <gilbert.alexandre.eng@gmai.com>
2023-08-22 23:52:17 -0300

    Criação dos Componentes basicos iniciais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { TitleBarComponent } from './components/title-bar/title-bar.component';
+import { CardComponent } from './components/card/card.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    TitleBarComponent,
+    CardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
